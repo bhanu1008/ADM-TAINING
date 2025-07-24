4cfc34b4e0f1506ceb15276cb1a8e88fcc4477f5
2431669 <2431669@LTIN604034.cts.com>
2025-07-24 16:48:08 +0530

    SECOND COMMIT

---
+++ STAR/src/Jexample/exp2.java
@@ -0,0 +1,14 @@
+package Jexample;
+
+public class exp2 {
+
+	public exp2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("STARS");
+
+	}
+
+}
