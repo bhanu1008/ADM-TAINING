2d49174d911370e83835585141bd6f6afb47911d
2431669 <2431669@LTIN604034.cts.com>
2025-07-24 15:02:44 +0530

    first commit

---
+++ STAR/src/Jexample/exp1.java
@@ -0,0 +1,15 @@
+package Jexample;
+
+public class exp1 {
+
+	public exp1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome");
+
+	}
+
+}
